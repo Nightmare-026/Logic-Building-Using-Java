ff40cd5dfccec15642506dea44bb3f0309d4414e
Nightmare <ganeshsharma7114@gmail.com>
2026-08-28 10:23:56 +0530

    Largest of Two java program added.

---
+++ basic_problems/largestOfTwo.java
@@ -0,0 +1,32 @@
+package basic_problems;
+
+import java.util.Scanner;
+
+public class largestOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        float num1 = sc.nextFloat();
+        System.out.print("Enter second number : ");
+        float num2 = sc.nextFloat();
+        if(num1 == num2){
+            System.out.println("Both are equal.");
+            return;
+        }
+
+        // // Using Ternary Operator
+        // float result = (num1 > num2) ? num1 : num2;
+        // System.out.println(result + " is greater.");
+
+        // Using if else
+        if(num1 > num2){
+            System.out.println(num1 + " is greater.");
+        } else{
+            System.out.println(num2 + " is greater.");
+        }
+
+
+        sc.close();
+    }
+}
