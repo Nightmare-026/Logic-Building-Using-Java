75244a1015201a89e9ad37620f47fa567a8bb93d
Nightmare <ganeshsharma7114@gmail.com>
2026-08-29 19:48:00 +0530

    Swap using XOR java program added.

--- basic_problems/SwapUsingXOR.java
+++ basic_problems/SwapUsingXOR.java
@@ -1,21 +1,22 @@
-// problem : Swap two numbers without using third variable or XOR.
+// Problem : Swap two numbers using XOR operator
 
 package basic_problems;
-
 import java.util.Scanner;
 
-public class SwapTwoNumbersWUTV {
+public class SwapUsingXOR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter first number : ");
         int num1 = sc.nextInt();
         System.out.print("Enter second number : ");
         int num2 = sc.nextInt();
 
-        num1 = num1 + num2 - (num2 = num1);
-        System.out.println("After swapping first number = " + num1 + " and second number = " + num2);
+        num1 = num1 ^ num2;
+        num2 = num1 ^ num2;
+        num1 = num1 ^ num2;
 
+        System.out.println("After swapping first number = " + num1 + " second number = " + num2);
         sc.close();
+
     }
 }
